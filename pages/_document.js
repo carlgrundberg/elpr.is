5c1d94d868f2f36d55575845fa29222e9227be19
Carl Grundberg <carl.grundberg@menmo.se>
2023-11-03 13:51:30 +0100

    icons

--- pages/_document.js
+++ pages/_document.js
@@ -7,9 +7,13 @@ class MyDocument extends Document {
         <Head>
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
           <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,700&display=swap" rel="stylesheet" />
-          <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css"></link>
-          {/* <script async defer data-website-id="ad73cb9d-1c16-402a-ab84-fb3124da762e" src="https://u.elpr.is/umami.js"></script> */}
-          <meta name="theme-color" content="#1e293b" />
+          <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" />
+          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+          <link rel="manifest" href="/site.webmanifest" />
+          <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#1e293b" />
+          <meta name="theme-color" content="#ffffff" />
         </Head>
         <body className='h-full grid place-content-center leading-normal tracking-normal text-gray-100 bg-slate-800'>
           <Main />
